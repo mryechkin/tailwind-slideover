99112f84dce856677ccb303434bc06c3380667a8
Mykhaylo Ryechkin <mykhaylo_ryechkin@manulife.ca>
2021-04-20 19:18:19 -0400

    Adding the HeadlessUI component version (new to Tailwind UI)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { OverlayProvider, usePreventScroll } from 'react-aria';
 import Button from './components/Button';
 import SlideOver from './components/SlideOver';
+import HeadlessSlideOver from './components/HeadlessSlideOver';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleOnClose = () => setIsOpen(false);
 
@@ -18,6 +20,9 @@ function App() {
           <div className="relative p-20 bg-white shadow-lg lg:rounded-3xl">
             <div className="max-w-md mx-auto lg:max-w-2xl">
               <Button onClick={() => setIsOpen(true)}>Click Me</Button>
+              <Button className="mx-2" onClick={() => setOpen(true)}>
+                Click Me Again
+              </Button>
               <p className="p-4">
                 Ipsum dolore cillum labore anim eu voluptate sunt est ea
                 aliquip. Irure consequat eiusmod minim consectetur ut magna
@@ -72,6 +77,18 @@ function App() {
                   </Button>
                 </div>
               </SlideOver>
+              <HeadlessSlideOver
+                open={open}
+                setOpen={setOpen}
+                title="Item Details"
+              >
+                <div className="flex flex-col">
+                  <input type="text" className="border-gray-300 rounded-md" />
+                  <Button className="mt-4" onClick={handleOnClose}>
+                    OK
+                  </Button>
+                </div>
+              </HeadlessSlideOver>
             </div>
           </div>
         </div>
--- src/components/SlideOver.js
+++ src/components/SlideOver.js
@@ -7,6 +7,7 @@ import {
   useDialog,
 } from 'react-aria';
 import { motion, AnimatePresence } from 'framer-motion';
+import { XIcon } from '@heroicons/react/outline';
 
 function SlideOverContent(props) {
   const { onClose, children, title } = props;
@@ -46,21 +47,7 @@ function SlideOverContent(props) {
                   className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
                 >
                   <span className="sr-only">Close panel</span>
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
               </div>
               <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll">
